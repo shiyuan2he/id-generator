35e0b1aa8211712f5f21a40c7a5db798f7661946
shiyuan2he <shiyuan4work@126.com>
2020-07-27 17:01:58 +0800

    基础版id生成器

---
+++ id-generator-base/src/main/java/org/hsy/id/generator/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 基本id生成器
+ * @author heshiyuan
+ * @date 2020/7/27 16:02
+ */
+package org.hsy.id.generator;
---
+++ id-generator-base/src/test/java/org/hsy/id/generator/IdHelperTest.java
@@ -0,0 +1,23 @@
+package org.hsy.id.generator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author heshiyuan
+ * @date 2020/7/27 16:10
+ */
+@Slf4j
+public class IdHelperTest {
+
+    @Test
+    public void nextId() {
+        IntStream.range(1, 2).forEach(count -> log.info("{}", IdHelper.nextId()));
+    }
+
+    @Test
+    public void stringIdToLongId() {
+    }
+}
